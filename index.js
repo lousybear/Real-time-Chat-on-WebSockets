7dc5846f0f84cec91c95ce12e221179f00a533b6
Ashish Kumar Senapati <ashishsenapati21@gmail.com>
2020-07-17 23:53:03 +0530

    updated users on socket

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const socket = require('socket.io');
 
 const port = 8000;
+const activeUsers = new Set();
 
 const app = express();
+
 app.use(express.static('public'))
 const server = app.listen(port, () => {
     console.log(`Listening on ${port}`)
 });
-socket(server).on('connection', () => {
+
+const io = socket(server)
+io.on('connection', () => {
+
     console.log(`Socket Connected`)
+
+    socket.on('new user', (data) => {
+        socket.userId = data;
+        activeUsers.add(data);
+        io.emit("new user", [...activeUsers])
+    });
+
+    socket.on('disconnect', () => {
+        activeUsers.delete(socket.userId);
+        io.emit("user disconnected", socket.userId);
+    })
 })
